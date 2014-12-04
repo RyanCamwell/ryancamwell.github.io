8e779f5b9c2dc30780e8883d114068c99ecb791b
RyanCamwell <ryancamwell91@gmail.com>
2014-12-04 23:00:55 +0000

    Empty

--- js/fade.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-$(".fader_img").fadeTo("slow", 0.0);
-$(".fader_img").hover(function(){
-$(this).fadeTo("slow", 1.0);
-},function(){
-$(this).fadeTo("slow", 0.0);
-});
-});
-
--- js/ie-fade.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-$(".fader_img").fadeTo("slow", 0.0);
-$(".fader_img").hover(function(){
-$(this).fadeTo("slow", 1);
-},function(){
-$(this).fadeTo("slow", 0.0);
-});
-});
