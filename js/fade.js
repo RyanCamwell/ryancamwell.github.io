73da3c79ba0aa99d7736f917b3ea20db128f1fc2
RyanCamwell <ryancamwell91@gmail.com>
2014-12-04 22:51:02 +0000

    New Website (entire switch)
    Complete new design

---
+++ js/fade.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+$(".fader_img").fadeTo("slow", 0.0);
+$(".fader_img").hover(function(){
+$(this).fadeTo("slow", 1.0);
+},function(){
+$(this).fadeTo("slow", 0.0);
+});
+});
+
