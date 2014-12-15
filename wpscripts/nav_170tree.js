cb5c7a3b5960c9ef767e30e2e249ecae0f099f37
RyanCamwell <ryancamwell91@gmail.com>
2014-12-15 21:40:05 +0000

    rename (decap)

--- wpscripts/nav_170tree.js
+++ wpscripts/nav_170tree.js
@@ -9,31 +9,31 @@ var nav_170tree = WpNavBar.readTree({
 {   'sTitle':'Portfolio',
 'bIsExternal':true,
 'bIsWebPath':true,
-'sUrl':'Portfolio.html',
+'sUrl':'portfolio.html',
 'sTarget':'_self',"childArray" : [
 {   'sTitle':'Favourites',
 'bIsExternal':true,
 'bIsWebPath':true,
-'sUrl':'Portfolio.html',
+'sUrl':'portfolio.html',
 'sTarget':'_self'
 },
 {   'sTitle':'Comic Con 22nd Nov 2014',
 'bIsExternal':true,
 'bIsWebPath':true,
-'sUrl':'Comic_Con.html',
+'sUrl':'comic_con.html',
 'sTarget':'_self'
 }]
 },
 {   'sTitle':'About',
 'bIsExternal':true,
 'bIsWebPath':true,
-'sUrl':'About.html',
+'sUrl':'about.html',
 'sTarget':'_self'
 },
 {   'sTitle':'Contact',
 'bIsExternal':true,
 'bIsWebPath':true,
-'sUrl':'Contact.html',
+'sUrl':'contact.html',
 'sTarget':'_self'
 }]
 });
